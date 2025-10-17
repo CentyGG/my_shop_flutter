3bd42c7822c8aa68935836803fd15251e1edc059
CentyGG <makarsyshchikov04@gmail.com>
2025-10-17 22:27:27 +0300

    Добавлена структура product.dart

--- lib/features/models/product.dart
+++ lib/features/models/product.dart
@@ -3,11 +3,23 @@ class Product {
   final String name;
   final String description;
   final double price;
-
   Product({
     required this.id,
     required this.name,
     required this.description,
     required this.price,
   });
+  Product copyWith({
+    String? id,
+    String? name,
+    String? description,
+    double? price,
+  }) {
+    return Product(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      description: description ?? this.description,
+      price: price ?? this.price,
+    );
+  }
 }
